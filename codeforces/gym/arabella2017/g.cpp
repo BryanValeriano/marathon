8ab459c43d292db95fbe4a7937b7e543d176b7c8
Bryan Valeriano <bryan.b.valeriano@gmail.com>
2019-09-30 08:51:04 -0300

    quick

--- codeforces/gym/arabella2017/g.cpp
+++ codeforces/gym/arabella2017/g.cpp
@@ -15,42 +15,40 @@ typedef vector<ii> vii;
 const int INF = 0x3f3f3f3f;
 const double PI = acos(-1.0);
 
-const int T = 1e4;
-int mat[T][T];
-int lastR[T];
-int lastC[T];
-int n,m,k;
+const int T = 22;
+
+ll n,m,k;
+ll x[T];
+ll y[T];
+
+ll solve(int gol) {
+    ll x1 = n;
+    ll y1 = m;
+    ll x2 = 0;
+    ll y2 = 0;
+    for(int i = 0; i < k; i++)
+        if(gol & (1 << i)) {
+            x1 = min(x[i],x1);
+            y1 = min(y[i],y1);
+            x2 = max(x[i],x2);
+            y2 = max(y[i],y2);
+        }
+
+    return (x1*y1) * (n-x2+1) * (m-y2+1);
+}
 
 int main() {
     ios_base::sync_with_stdio(false);
     int tc; cin >> tc;
     while(tc--) {
         cin >> n >> m >> k;
-        for(int i = 1; i <= n; i++)
-            for(int j = 1; j <= m; j++)
-                mat[i][j] = 1;
-
-        while(k--) {
-            int a,b;cin >> a >> b;
-            mat[a][b] = 0;
-            lastR[b] = a;
-            lastC[a] = b;
+        for(int i = 0; i < k; i++) cin >> x[i] >> y[i];
+        ll ans = ((n*(n+1)/2)) * ((m*(m+1)/2));
+        int gol = pow(2,k)-1;
+        for(int i = 1; i <= gol; i++) {
+            if(__builtin_popcount(i)&1) ans -= solve(i);
+            else ans += solve(i);
         }
-
-        int ans = 0;
-
-        for(int i = 1; i <= n; i++) {
-            lastR[i] = max(lastR[i], lastR[i-1]);
-            for(int j = 1; j <= m; j++) {
-                lastC[j] = max(lastC[j], lastC[j-1]);
-                if(mat[i][j] == 0) { cout << 0 << " "; continue; }
-                mat[i][j] += mat[i-1][j] + mat[i][j-1] - (mat[i-1][j-1] - mat[lastR[i]][lastC[j]]);
-                if(tc == 1) cout << mat[i][j] << " ";
-                ans += mat[i][j];
-            }
-            if(tc == 1) cout << endl;
-        }
-
         cout << ans << endl;
     }
 
