7fee7c787ca3f65c8cee7d6f0db64cb271160724
Bryan Valeriano <bryan.b.valeriano@gmail.com>
2019-10-23 09:04:29 -0300

    quick

--- codeforces/speedB9/b.cpp
+++ codeforces/speedB9/b.cpp
@@ -12,40 +12,32 @@ typedef pair<int,int> ii;
 typedef vector< pair<int,int> > vii;
 const int INF = 0x3f3f3f3f;
 
-ll dp[20][20];
-int vis[20][20];
-int vez;
-string s;
+const int T = 20;
+vector<ll> bag;
+ll v[T];
 
-ll solve(int at, int sum) {
-    if(sum > 3) return 0;
-    if(at == 20) return 1;
-    if(vis[at][sum] == vez) return dp[at][sum];
-
-    ll ans = 0;
-    for(int i = s[at]-'0'; i <= 9; i++)
-        ans += solve(at+1, sum + (i == 0));
-    
-    vis[at][sum] = vez;
-    return dp[at][sum] = ans;
+void get(ll x, int casa, int f) {
+    if(f > 3 or x > 1e18) return;
+    if(casa == 19) { bag.pb(x); return; }
+    for(ll i = 0; i <= (casa == 18? 1 : 9); i++) get(x + i*v[casa], casa+1, f + (i != 0));
 }
- 
+
+
 int main() {
     ios_base::sync_with_stdio(false);
+    v[0] = 1;
+    for(int i = 1; i <= 18; i++) v[i] = (10ll * v[i-1]);
+    get(0,0,0);
+    sort(bag.begin(), bag.end());
+
     int tc; cin >> tc;
     while(tc--) {
-        ll a,b; cin >> a >> b;
-
-        vez++;
-        s = to_string(a);
-        while(s.size() < 20) s += '0';
-        ll x = solve(0,0);
-
-        vez++;
-        s = to_string(b);
-        while(s.size() < 20) s += '0';
-        ll y = solve(0,0);
-        
+        ll a,b;
+        cin >> a >> b;
+        int x = lower_bound(bag.begin(),bag.end(),a) - bag.begin();
+        int y = lower_bound(bag.begin(),bag.end(),b+1) - bag.begin();
+        x = bag.size() - x;
+        y = bag.size() - y;
         cout << x-y << endl;
     }
     return 0;
